42aa6b749d30f2297b3ddeaaed53e354b13d5349
Kezi <keziolio123@gmail.com>
2024-08-22 02:05:10 +0200

    add infrared receiver

--- antani_sw/src/main.rs
+++ antani_sw/src/main.rs
@@ -1,9 +1,13 @@
 #![no_std]
 #![no_main]
 
+use core::cell::RefCell;
+
+use cortex_m::interrupt::Mutex;
 use defmt::*;
 use defmt_rtt as _;
 use embedded_hal::digital::StatefulOutputPin;
+use infrared::{protocol::NecDebug, Receiver};
 use panic_probe as _;
 
 #[link_section = ".boot2"]
@@ -11,10 +15,18 @@ use panic_probe as _;
 pub static BOOT_LOADER: [u8; 256] = rp2040_boot2::BOOT_LOADER_W25Q080;
 
 use rp2040_hal::{
-    clocks::init_clocks_and_plls, entry, gpio::Pins, pac, pio::PIOExt, Clock, Sio, Timer, Watchdog,
+    clocks::init_clocks_and_plls,
+    entry,
+    gpio::{bank0::Gpio10, FunctionSio, Pins, PullNone, SioInput},
+    pac::{self, interrupt},
+    pio::PIOExt,
+    Clock, Sio, Timer, Watchdog,
 };
 use smart_leds::RGB8;
 use ws2812_pio::Ws2812;
+
+use rp2040_hal::gpio::Interrupt::EdgeHigh;
+use rp2040_hal::gpio::Interrupt::EdgeLow;
 
 const LED_MATRIX_WIDTH: usize = 3;
 const LED_MATRIX_HEIGHT: usize = 3;
@@ -59,6 +71,14 @@ impl LedMatrix {
     }
 }
 
+static G_TIMER: Mutex<RefCell<Option<rp2040_hal::Timer>>> = Mutex::new(RefCell::new(None));
+
+static G_INTERRUPT_RECEIVER: Mutex<
+    RefCell<
+        Option<Receiver<NecDebug, rp2040_hal::gpio::Pin<Gpio10, FunctionSio<SioInput>, PullNone>>>,
+    >,
+> = Mutex::new(RefCell::new(None));
+
 #[entry]
 fn main() -> ! {
     info!("Program start");
@@ -92,13 +112,37 @@ fn main() -> ! {
 
     let timer = Timer::new(pac.TIMER, &mut pac.RESETS, &clocks);
 
+    #[allow(unsafe_code)]
+    unsafe {
+        pac::NVIC::unmask(pac::Interrupt::TIMER_IRQ_0);
+        pac::NVIC::unmask(pac::Interrupt::IO_IRQ_BANK0);
+    }
+
     let (mut pio, sm0, _, _, _) = pac.PIO0.split(&mut pac.RESETS);
 
     let mut ir_blaster = pins.gpio11.into_push_pull_output();
-    #[allow(unused_variables)]
-    let ir_sensor = pins.gpio10.as_input();
-    #[allow(unused_variables)]
-    let user_button = pins.gpio14.into_pull_up_input();
+    let ir_sensor = pins.gpio10.into_floating_input();
+    let _user_button = pins.gpio9.into_pull_up_input();
+
+    let int_receiver = Receiver::builder()
+        .rc5()
+        .frequency(1_000_000)
+        .pin(ir_sensor)
+        .protocol()
+        .build();
+
+    cortex_m::interrupt::free(|cs| {
+        G_TIMER.borrow(cs).replace(Some(timer));
+        G_INTERRUPT_RECEIVER.borrow(cs).replace(Some(int_receiver));
+
+        if let Some(int_receiver) = G_INTERRUPT_RECEIVER.borrow(cs).borrow_mut().as_mut() {
+            let pin = int_receiver.pin_mut();
+            pin.set_interrupt_enabled(EdgeLow, true);
+            pin.set_interrupt_enabled(EdgeHigh, true);
+        }
+    });
+
+    println!("Starting loop");
 
     let mut ws = Ws2812::new(
         pins.gpio19.into_function(),
@@ -120,7 +164,7 @@ fn main() -> ! {
     let blue: RGB8 = (0, 0, 255).into();
     let black: RGB8 = (0, 0, 0).into();
 
-    // clear all leds
+    // clear all leds (if we are using a strip for debug)
     ws.write(core::iter::repeat(black).take(400)).unwrap();
 
     loop {
@@ -141,4 +185,21 @@ fn main() -> ! {
 
         ir_blaster.toggle().unwrap();
     }
+}
+
+#[interrupt]
+fn IO_IRQ_BANK0() {
+    cortex_m::interrupt::free(|cs| {
+        if let Some(int_receiver) = G_INTERRUPT_RECEIVER.borrow(cs).borrow_mut().as_mut() {
+            if let Some(timer) = G_TIMER.borrow(cs).borrow_mut().as_mut() {
+                if let Ok(Some(cmd)) = int_receiver.event_instant(timer.get_counter_low()) {
+                    println!("Action: {:?} ", cmd.bits);
+                }
+            }
+
+            let pin = int_receiver.pin_mut();
+            pin.clear_interrupt(EdgeLow);
+            pin.clear_interrupt(EdgeHigh);
+        }
+    });
 }
