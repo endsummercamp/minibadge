5b59a477bf3a3b3e9d044d8f5588a0d3d1281953
Kezi <keziolio123@gmail.com>
2024-08-25 16:53:38 +0200

    [software] add rgb effects / animations

--- antani_sw/src/main.rs
+++ antani_sw/src/main.rs
@@ -1,46 +1,58 @@
 #![no_std]
 #![no_main]
 
+use core::f64;
+
 use defmt::*;
 use defmt_rtt as _;
 use embassy_executor::{InterruptExecutor, Spawner};
 use embassy_rp::adc;
-use embassy_rp::dma;
 use embassy_rp::gpio::Input;
 use embassy_rp::gpio::Pull;
 use embassy_rp::interrupt;
 use embassy_rp::interrupt::{InterruptExt, Priority};
 
 use embassy_rp::peripherals::PIO0;
-use embassy_rp::pio::{
-    Common, Config, FifoJoin, Instance, InterruptHandler, Pio, PioPin, ShiftConfig, ShiftDirection,
-    StateMachine,
-};
-use embassy_rp::{bind_interrupts, clocks, into_ref, Peripheral, PeripheralRef};
+use embassy_rp::pio::{InterruptHandler, Pio};
 use embassy_sync::blocking_mutex::raw::CriticalSectionRawMutex;
 
 use embassy_sync::channel::{Channel, Sender};
+use embassy_sync::lazy_lock::LazyLock;
 use embassy_time::Instant;
 use embassy_time::{Duration, Ticker, Timer};
-use fixed::types::U24F8;
-use fixed_macro::fixed;
+
+use embassy_rp::bind_interrupts;
+use heapless::Vec;
 use infrared::{protocol::NecDebug, Receiver};
 use num_traits::real::Real;
 use panic_probe as _;
+
+mod ws2812;
 
 bind_interrupts!(struct Irqs {
     PIO0_IRQ_0 => InterruptHandler<PIO0>;
     ADC_IRQ_FIFO => adc::InterruptHandler;
 });
 
+use rand::rngs::SmallRng;
+use rand::Rng;
+use rand::SeedableRng;
 use smart_leds::RGB8;
+use ws2812::Ws2812;
 
 const LED_MATRIX_WIDTH: usize = 3;
 const LED_MATRIX_HEIGHT: usize = 3;
 const LED_MATRIX_SIZE: usize = LED_MATRIX_WIDTH * LED_MATRIX_HEIGHT;
 
+#[derive(Clone, Copy)]
 struct LedPattern {
     pattern: u16,
+}
+
+impl LedPattern {
+    const fn new(pattern: u16) -> Self {
+        Self { pattern }
+    }
 }
 
 impl From<u16> for LedPattern {
@@ -49,17 +61,22 @@ impl From<u16> for LedPattern {
     }
 }
 
-const GLIDER_PATTERN: LedPattern = LedPattern {
-    pattern: 0b010001111,
-};
+struct AnimationPattern {
+    patterns: Vec<LedPattern, 20>,
+}
 
-const ALL_ON_PATTERN: LedPattern = LedPattern {
-    pattern: 0b111111111,
-};
+impl AnimationPattern {
+    fn new(patterns: &[u16]) -> Self {
+        Self {
+            patterns: patterns.iter().map(|&p| LedPattern::new(p)).collect(),
+        }
+    }
+}
 
 struct LedMatrix {
     pub framebuffer: [RGB8; LED_MATRIX_SIZE],
     gain: f32,
+    override_gain: f32,
 }
 
 #[allow(dead_code)]
@@ -68,11 +85,20 @@ impl LedMatrix {
         Self {
             framebuffer: [(0, 0, 0).into(); LED_MATRIX_SIZE],
             gain: 0.5,
+            override_gain: 1.0,
         }
     }
 
     fn set_gain(&mut self, gain: f32) {
         self.gain = gain;
+    }
+
+    fn set_override_gain(&mut self, gain: f32) {
+        self.override_gain = gain;
+    }
+
+    fn get_gain(&self) -> f32 {
+        self.gain * self.override_gain
     }
 
     fn set_pixel(&mut self, x: usize, y: usize, rgb: RGB8) {
@@ -87,64 +113,80 @@ impl LedMatrix {
         }
     }
 
+    fn clear(&mut self) {
+        self.set_all((0, 0, 0).into());
+    }
+
     fn blit(&mut self) -> impl Iterator<Item = RGB8> + '_ {
         self.framebuffer.iter().map(|rgb| {
-            let r = (rgb.r as f32 * self.gain) as u8;
-            let g = (rgb.g as f32 * self.gain) as u8;
-            let b = (rgb.b as f32 * self.gain) as u8;
+            let r = (rgb.r as f32 * self.get_gain()) as u8;
+            let g = (rgb.g as f32 * self.get_gain()) as u8;
+            let b = (rgb.b as f32 * self.get_gain()) as u8;
             (r, g, b).into()
         })
     }
 
-    fn render(&mut self, pattern: LedPattern, colour: RGB8) {
-        // this garbage is to rotate the pattern so the usb port is at the top
-        // i hope the compiler optimizes this out
-
+    fn render(&mut self, pattern: &LedPattern, colour: RGB8) {
         let colour = RGB8 {
-            r: (colour.r as f32 * self.gain) as u8,
-            g: (colour.g as f32 * self.gain) as u8,
-            b: (colour.b as f32 * self.gain) as u8,
+            r: (colour.r as f32 * self.get_gain()) as u8,
+            g: (colour.g as f32 * self.get_gain()) as u8,
+            b: (colour.b as f32 * self.get_gain()) as u8,
         };
 
-        if pattern.pattern & 0b100000000 != 0 {
-            self.set_pixel(2, 0, colour);
-        }
+        // this maps bits in the pattern bitfield to the corresponding led in the matrix
+        let bit_offsets = [
+            (0, 2), // bit 0, first led
+            (0, 1),
+            (0, 0),
+            (1, 2),
+            (1, 1),
+            (1, 0),
+            (2, 2),
+            (2, 1),
+            (2, 0), // bit 8, the last led
+        ];
 
-        if pattern.pattern & 0b010000000 != 0 {
-            self.set_pixel(2, 1, colour);
-        }
-
-        if pattern.pattern & 0b001000000 != 0 {
-            self.set_pixel(2, 2, colour);
-        }
-
-        if pattern.pattern & 0b000100000 != 0 {
-            self.set_pixel(1, 0, colour);
-        }
-
-        if pattern.pattern & 0b000010000 != 0 {
-            self.set_pixel(1, 1, colour);
-        }
-
-        if pattern.pattern & 0b000001000 != 0 {
-            self.set_pixel(1, 2, colour);
-        }
-
-        if pattern.pattern & 0b000000100 != 0 {
-            self.set_pixel(0, 0, colour);
-        }
-
-        if pattern.pattern & 0b000000010 != 0 {
-            self.set_pixel(0, 1, colour);
-        }
-
-        if pattern.pattern & 0b000000001 != 0 {
-            self.set_pixel(0, 2, colour);
+        for (i, (x, y)) in bit_offsets.iter().enumerate() {
+            if pattern.pattern & (1 << i) != 0 {
+                self.set_pixel(*x, *y, colour);
+            }
         }
     }
 }
 
-fn hsl2rgb(h: f64, s: f64, l: f64) -> (u8, u8, u8) {
+#[derive(Clone)]
+struct RenderCommand {
+    effect: RunEffect,
+    color: ColorPalette,
+    color_shaders: Vec<FragmentShader, 8>,
+}
+
+struct RenderManager {
+    mtrx: LedMatrix,
+    rng: SmallRng,
+}
+
+impl RenderManager {
+    fn render_single(&mut self, command: &RenderCommand, t: f64) {
+        let mut color = command.color.render(t);
+
+        for shader in command.color_shaders.iter() {
+            color = shader.render(t, color);
+        }
+
+        let pattern = command.effect.render(t, self);
+
+        self.mtrx.render(&pattern, color);
+    }
+
+    fn render(&mut self, command: &[RenderCommand], t: f64) {
+        for c in command.iter() {
+            self.render_single(c, t);
+        }
+    }
+}
+
+fn hsl2rgb(h: f64, s: f64, l: f64) -> RGB8 {
     let h = h * 360.0;
     let c = (1.0 - (2.0 * l - 1.0).abs()) * s;
     let x = c * (1.0 - ((h / 60.0) % 2.0 - 1.0).abs());
@@ -164,106 +206,119 @@ fn hsl2rgb(h: f64, s: f64, l: f64) -> (u8, u8, u8) {
     let g = ((g + m) * 255.0).round() as u8;
     let b = ((b + m) * 255.0).round() as u8;
 
-    (r, g, b)
-}
-pub struct Ws2812<'d, P: Instance, const S: usize, const N: usize> {
-    dma: PeripheralRef<'d, dma::AnyChannel>,
-    sm: StateMachine<'d, P, S>,
+    (r, g, b).into()
 }
 
-impl<'d, P: Instance, const S: usize, const N: usize> Ws2812<'d, P, S, N> {
-    pub fn new(
-        pio: &mut Common<'d, P>,
-        mut sm: StateMachine<'d, P, S>,
-        dma: impl Peripheral<P = impl dma::Channel> + 'd,
-        pin: impl PioPin,
-    ) -> Self {
-        into_ref!(dma);
+#[derive(Clone)]
+enum FragmentShader {
+    Breathing(f32), // speed
+    Blinking(f32),  // speed
+}
 
-        // Setup sm0
-
-        // prepare the PIO program
-        let side_set = pio::SideSet::new(false, 1, false);
-        let mut a: pio::Assembler<32> = pio::Assembler::new_with_side_set(side_set);
-
-        const T1: u8 = 2; // start bit
-        const T2: u8 = 5; // data bit
-        const T3: u8 = 3; // stop bit
-        const CYCLES_PER_BIT: u32 = (T1 + T2 + T3) as u32;
-
-        let mut wrap_target = a.label();
-        let mut wrap_source = a.label();
-        let mut do_zero = a.label();
-        a.set_with_side_set(pio::SetDestination::PINDIRS, 1, 0);
-        a.bind(&mut wrap_target);
-        // Do stop bit
-        a.out_with_delay_and_side_set(pio::OutDestination::X, 1, T3 - 1, 0);
-        // Do start bit
-        a.jmp_with_delay_and_side_set(pio::JmpCondition::XIsZero, &mut do_zero, T1 - 1, 1);
-        // Do data bit = 1
-        a.jmp_with_delay_and_side_set(pio::JmpCondition::Always, &mut wrap_target, T2 - 1, 1);
-        a.bind(&mut do_zero);
-        // Do data bit = 0
-        a.nop_with_delay_and_side_set(T2 - 1, 0);
-        a.bind(&mut wrap_source);
-
-        let prg = a.assemble_with_wrap(wrap_source, wrap_target);
-        let mut cfg = Config::default();
-
-        // Pin config
-        let out_pin = pio.make_pio_pin(pin);
-        cfg.set_out_pins(&[&out_pin]);
-        cfg.set_set_pins(&[&out_pin]);
-
-        cfg.use_program(&pio.load_program(&prg), &[&out_pin]);
-
-        // Clock config, measured in kHz to avoid overflows
-        // TODO CLOCK_FREQ should come from embassy_rp
-        let clock_freq = U24F8::from_num(clocks::clk_sys_freq() / 1000);
-        let ws2812_freq = fixed!(800: U24F8);
-        let bit_freq = ws2812_freq * CYCLES_PER_BIT;
-        cfg.clock_divider = clock_freq / bit_freq;
-
-        // FIFO config
-        cfg.fifo_join = FifoJoin::TxOnly;
-        cfg.shift_out = ShiftConfig {
-            auto_fill: true,
-            threshold: 24,
-            direction: ShiftDirection::Left,
-        };
-
-        sm.set_config(&cfg);
-        sm.set_enable(true);
-
-        Self {
-            dma: dma.map_into(),
-            sm,
+impl FragmentShader {
+    fn render(&self, t: f64, color: RGB8) -> RGB8 {
+        match self {
+            FragmentShader::Breathing(speed) => {
+                let t = (t * *speed as f64) % 1.0;
+                let l = 0.5 + 0.5 * (2.0 * f64::consts::PI * t).sin();
+                let c = (color.r as f64 * l, color.g as f64 * l, color.b as f64 * l);
+                (c.0 as u8, c.1 as u8, c.2 as u8).into()
+            }
+            FragmentShader::Blinking(speed) => {
+                let t = (t * *speed as f64) % 1.0;
+                if t < 0.5 {
+                    color
+                } else {
+                    (0, 0, 0).into()
+                }
+            }
         }
     }
+}
 
-    pub async fn write(&mut self, colors: &[RGB8; N]) {
-        // Precompute the word bytes from the colors
-        let mut words = [0u32; N];
-        for i in 0..N {
-            let word = (u32::from(colors[i].g) << 24)
-                | (u32::from(colors[i].r) << 16)
-                | (u32::from(colors[i].b) << 8);
-            words[i] = word;
+#[allow(dead_code)]
+#[derive(Clone)]
+enum ColorPalette {
+    Rainbow(f32, f32), // speed, phase
+    Solid(RGB8),
+    Custom(Vec<RGB8, 16>, f32), // palette, speed
+}
+
+impl ColorPalette {
+    fn render(&self, t: f64) -> RGB8 {
+        match self {
+            ColorPalette::Rainbow(speed, phase) => {
+                hsl2rgb((t * *speed as f64 + *phase as f64) % 1.0, 1.0, 0.5)
+            }
+            ColorPalette::Solid(rgb) => *rgb,
+            ColorPalette::Custom(palette, speed) => {
+                let idx = (t * *speed as f64) as usize % palette.len();
+                palette[idx]
+            }
         }
-
-        // DMA transfer
-        self.sm.tx().dma_push(self.dma.reborrow(), &words).await;
-
-        Timer::after_micros(55).await;
     }
 }
 
-type AntaniSender = Sender<'static, CriticalSectionRawMutex, AntaniCommand, 8>;
-enum AntaniCommand {
+#[allow(dead_code)]
+#[derive(Clone)]
+enum RunEffect {
+    SimplePattern(LedPattern),
+    AnimationPattern(&'static AnimationPattern, f32), // pattern, speed
+    ReverseAnimationPattern(&'static AnimationPattern, f32), // pattern, speed
+    AnimationRandom(&'static AnimationPattern),       // pattern
+}
+
+impl RunEffect {
+    fn render(&self, t: f64, renderman: &mut RenderManager) -> LedPattern {
+        match self {
+            RunEffect::SimplePattern(pattern) => *pattern,
+            RunEffect::AnimationPattern(pattern, speed) => {
+                let idx = (t * *speed as f64) as usize % pattern.patterns.len();
+                let pattern = &pattern.patterns[idx];
+                *pattern
+            }
+            RunEffect::ReverseAnimationPattern(pattern, speed) => {
+                let idx = (t * *speed as f64) as usize % pattern.patterns.len();
+                let pattern = &pattern.patterns[pattern.patterns.len() - idx - 1];
+                *pattern
+            }
+            RunEffect::AnimationRandom(pattern) => {
+                let idx = renderman.rng.gen_range(0..pattern.patterns.len());
+                let pattern = &pattern.patterns[idx];
+                *pattern
+            }
+        }
+    }
+}
+
+type AppSender = Sender<'static, CriticalSectionRawMutex, AppCommand, 8>;
+enum AppCommand {
     ThermalThrottleMultiplier(f32), // 1.0 = no throttle, 0.0 = full throttle
     IrCommand(u32),
 }
-static CHANNEL: Channel<CriticalSectionRawMutex, AntaniCommand, 8> = Channel::new();
+static CHANNEL: Channel<CriticalSectionRawMutex, AppCommand, 8> = Channel::new();
+
+struct Patterns {
+    pub glider: LedPattern,
+    pub all_on: LedPattern,
+    pub everything_once: AnimationPattern,
+}
+
+static PATTERNS: LazyLock<Patterns> = LazyLock::new(|| Patterns {
+    glider: LedPattern::new(0b010001111),
+    all_on: LedPattern::new(0b111111111),
+    everything_once: AnimationPattern::new(&[
+        0b100000000,
+        0b010000000,
+        0b001000000,
+        0b000100000,
+        0b000010000,
+        0b000001000,
+        0b000000100,
+        0b000000010,
+        0b000000001,
+    ]),
+});
 
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
@@ -286,40 +341,89 @@ async fn main(spawner: Spawner) {
     let highpriority_spawner = EXECUTOR_HIGH.start(interrupt::SWI_IRQ_1);
     unwrap!(highpriority_spawner.spawn(ir_receiver(ir_sensor, CHANNEL.sender())));
 
-    let mut mtrx = LedMatrix::new();
+    let mut renderman = RenderManager {
+        mtrx: LedMatrix::new(),
+        rng: SmallRng::seed_from_u64(69420),
+    };
 
     let mut ws2812 = Ws2812::new(&mut common, sm0, p.DMA_CH0, p.PIN_19);
 
+    let patterns = PATTERNS.get();
+
     println!("Starting loop");
 
-    let mut t;
+    let scenes: [Vec<RenderCommand, 8>; 3] = [
+        // strobing glider
+        Vec::from_slice(&[RenderCommand {
+            effect: RunEffect::SimplePattern(patterns.glider),
+            color: ColorPalette::Solid((0, 0, 255).into()),
+            color_shaders: Vec::from_slice(&[
+                FragmentShader::Breathing(0.7),
+                FragmentShader::Blinking(10.0),
+            ])
+            .unwrap(),
+        }])
+        .unwrap(),
+        // glider with particles
+        Vec::from_slice(&[
+            RenderCommand {
+                effect: RunEffect::SimplePattern(patterns.glider),
+                color: ColorPalette::Solid((0, 0, 255).into()),
+                color_shaders: Vec::from_slice(&[FragmentShader::Breathing(0.7)]).unwrap(),
+            },
+            RenderCommand {
+                effect: RunEffect::AnimationPattern(&patterns.everything_once, 6.0),
+                color: ColorPalette::Rainbow(0.25, 0.0),
+                color_shaders: Vec::new(),
+            },
+            RenderCommand {
+                effect: RunEffect::ReverseAnimationPattern(&patterns.everything_once, 6.0),
+                color: ColorPalette::Rainbow(0.25, 0.5),
+                color_shaders: Vec::new(),
+            },
+        ])
+        .unwrap(),
+        // double rainbow glider
+        Vec::from_slice(&[
+            RenderCommand {
+                effect: RunEffect::SimplePattern(patterns.all_on),
+                color: ColorPalette::Rainbow(0.25, 0.0),
+                color_shaders: Vec::new(),
+            },
+            RenderCommand {
+                effect: RunEffect::SimplePattern(patterns.glider),
+                color: ColorPalette::Rainbow(0.25, 0.5),
+                color_shaders: Vec::new(),
+            },
+        ])
+        .unwrap(),
+    ];
+
     loop {
         //t = timer.get_counter().ticks() as f64 / 1_000_000.0;
-        t = Instant::now().as_micros() as f64 / 1_000_000.0;
+        let t = Instant::now().as_micros() as f64 / 1_000_000.0;
 
+        renderman.mtrx.set_gain(0.5);
 
         if let Ok(ch) = CHANNEL.try_receive() {
             match ch {
-                AntaniCommand::ThermalThrottleMultiplier(gain) => {
-                    mtrx.set_gain(gain);
+                AppCommand::ThermalThrottleMultiplier(gain) => {
+                    renderman.mtrx.set_override_gain(gain);
                     println!("Thermal throttle multiplier: {}", gain);
                 }
-                AntaniCommand::IrCommand(cmd) => {
+                AppCommand::IrCommand(cmd) => {
                     println!("IR command: {}", cmd);
                 }
             }
         }
 
-        let color = hsl2rgb((t * 0.25) % 1.0, 1.0, 0.5);
-        let color2 = hsl2rgb((t * 0.25 + 0.5) % 1.0, 1.0, 0.5);
+        // change scene every 5 seconds
+        let scene_id = (t as u64 / 5) % scenes.len() as u64;
+        renderman.render(&scenes[scene_id as usize], t);
 
-        mtrx.render(ALL_ON_PATTERN, color.into());
-        mtrx.render(GLIDER_PATTERN, color2.into());
-        ws2812.write(&mtrx.framebuffer).await;
-
+        ws2812.write(&renderman.mtrx.framebuffer).await;
         Timer::after_millis(1).await;
-
-        //ir_blaster.toggle().unwrap();
+        renderman.mtrx.clear();
     }
 }
 
@@ -331,7 +435,7 @@ unsafe fn SWI_IRQ_1() {
 }
 
 #[embassy_executor::task]
-async fn ir_receiver(ir_sensor: Input<'static>, control: AntaniSender) {
+async fn ir_receiver(ir_sensor: Input<'static>, control: AppSender) {
     let mut int_receiver: Receiver<NecDebug, embassy_rp::gpio::Input> = Receiver::builder()
         .rc5()
         .frequency(1_000_000)
@@ -343,7 +447,7 @@ async fn ir_receiver(ir_sensor: Input<'static>, control: AntaniSender) {
         int_receiver.pin_mut().wait_for_any_edge().await;
 
         if let Ok(Some(cmd)) = int_receiver.event_instant(Instant::now().as_ticks() as u32) {
-            control.send(AntaniCommand::IrCommand(cmd.bits)).await;
+            control.send(AppCommand::IrCommand(cmd.bits)).await;
         }
     }
 }
@@ -352,7 +456,7 @@ async fn ir_receiver(ir_sensor: Input<'static>, control: AntaniSender) {
 async fn temperature(
     mut adc: adc::Adc<'static, adc::Async>,
     mut ts: adc::Channel<'static>,
-    control: AntaniSender,
+    control: AppSender,
 ) {
     let mut ticker = Ticker::every(Duration::from_secs(1));
 
@@ -368,7 +472,7 @@ async fn temperature(
             let gain: f64 = 1.0 - (temp_degrees_c - 55.0) / 10.0;
             let gain = gain.clamp(0.0, 1.0);
             control
-                .send(AntaniCommand::ThermalThrottleMultiplier(gain as f32))
+                .send(AppCommand::ThermalThrottleMultiplier(gain as f32))
                 .await;
         }
 
